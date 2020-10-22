24fa9a97435c027cb8fcd88d7b99b6e552cdae4f
jenny_malmefeldt <jennymalmefeldt@gmail.com>
2020-10-22 10:31:33 +0200

    Fetching the 10 series, using map function, styling, adding footer

--- src/components/DisplaySeries.jsx
+++ src/components/DisplaySeries.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 
 const DisplaySeries = () => {
-  const [series, setSeries] = useState();
+  const [series, setSeries] = useState([]);
 
   useEffect(() => {
     fetchSeries();
@@ -12,19 +12,26 @@ const DisplaySeries = () => {
     let response = await axios.get(
       "https://cors-anywhere.herokuapp.com/https://content.viaplay.se/pc-se/serier/samtliga"
     );
+    debugger;
     setSeries(
       response.data._embedded["viaplay:blocks"][0]._embedded["viaplay:products"]
     );
   };
 
   let seriesList = series.map((serie) => {
-    <img
-      src={serie.content.images.landscape.url}
-      alt={serie.id}
-      key={serie.id}
-    />;
+    return (
+      <div className="display-show">
+        <img
+          src={serie.content.images.landscape.url}
+          alt={serie.id}
+          key={serie.id}
+        />
+        ;
+      </div>
+    );
   });
 
-  return <div className="display-show">{seriesList}</div>;
+  return <div>{seriesList}</div>;
 };
+
 export default DisplaySeries;
